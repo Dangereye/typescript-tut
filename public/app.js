fada30c2e90fe48c07d2da395a12fea6499a4970
Craig Puxty <craig_puxty@talktalk.net>
2022-10-02 13:45:15 +0100

    Seperated Invoice into a module.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,5 @@
-"use strict";
 // Classes
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}.`;
-    }
-}
+import { Invoice } from "./modules/Invoice.js";
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,5 @@
 // Classes
-class Invoice {
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}.`;
-  }
-}
+import { Invoice } from "./modules/Invoice.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
