3972de0e7d54f680b618e2fb27f7271c0c358b7a
Craig Puxty <craig_puxty@talktalk.net>
2022-10-02 14:47:26 +0100

    Updated submit event to use the Invoice or Payment modules.

--- public/app.js
+++ public/app.js
@@ -1,22 +1,6 @@
 // Modules
 import { Invoice } from "./modules/Invoice.js";
 import { Payment } from "./modules/Payment.js";
-let docOne;
-let docTwo;
-docOne = new Invoice("yoshi", "web work", 250);
-docTwo = new Payment("mario", "plumbing work", 300);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-console.log(docs);
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.format());
-});
 // Form
 const form = document.querySelector(".new-item-form");
 // Inputs
@@ -27,5 +11,12 @@ const amount = document.querySelector("#amount");
 // Events
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, +amount.value);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(toFrom.value, details.value, +amount.value);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, +amount.value);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -5,29 +5,6 @@ import { HasFormatter } from "./interfaces/HasFormatter.js";
 import { Invoice } from "./modules/Invoice.js";
 import { Payment } from "./modules/Payment.js";
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice("yoshi", "web work", 250);
-docTwo = new Payment("mario", "plumbing work", 300);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
-console.log(docs);
-
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach((inv) => {
-  console.log(inv.format());
-});
-
 // Form
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -40,5 +17,11 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 // Events
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
-  console.log(type.value, toFrom.value, details.value, +amount.value);
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(toFrom.value, details.value, +amount.value);
+  } else {
+    doc = new Payment(toFrom.value, details.value, +amount.value);
+  }
+  console.log(doc);
 });
